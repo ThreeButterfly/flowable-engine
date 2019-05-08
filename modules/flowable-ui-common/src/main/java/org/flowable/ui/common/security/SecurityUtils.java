cc57b0cb72245fee9f3817061c203f4d6ff3a924
a157775541 <157775541@qq.com>
2019-05-08 16:58:07 +0800

    去除认证，让spring security不拦截请求

--- modules/flowable-ui-common/src/main/java/org/flowable/ui/common/security/SecurityUtils.java
+++ modules/flowable-ui-common/src/main/java/org/flowable/ui/common/security/SecurityUtils.java
@@ -13,9 +13,13 @@
 package org.flowable.ui.common.security;
 
 import org.flowable.idm.api.User;
+import org.flowable.ui.common.model.RemoteUser;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Utility class for Spring Security.
@@ -46,11 +50,25 @@ public class SecurityUtils {
             return assumeUser;
         }
 
-        User user = null;
-        FlowableAppUser appUser = getCurrentFlowableAppUser();
-        if (appUser != null) {
-            user = appUser.getUserObject();
-        }
+//        User user = null;
+//        FlowableAppUser appUser = getCurrentFlowableAppUser();
+//        if (appUser != null) {
+//            user = appUser.getUserObject();
+//        }
+        RemoteUser user = new RemoteUser();
+        user.setId("admin");
+        user.setDisplayName("admin");
+        user.setFirstName("admin");
+        user.setLastName("admin");
+        user.setEmail("admin@admin.com");
+        user.setPassword("test");
+        List<String> pris = new ArrayList<>();
+        pris.add(DefaultPrivileges.ACCESS_MODELER);
+        pris.add(DefaultPrivileges.ACCESS_IDM);
+        pris.add(DefaultPrivileges.ACCESS_ADMIN);
+        pris.add(DefaultPrivileges.ACCESS_TASK);
+        pris.add(DefaultPrivileges.ACCESS_REST_API);
+        user.setPrivileges(pris);
         return user;
     }
 
