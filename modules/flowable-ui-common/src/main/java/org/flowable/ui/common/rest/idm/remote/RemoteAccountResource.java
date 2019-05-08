180261c94eebeded660f915ed6b200ab6cd4df51
a157775541 <157775541@qq.com>
2019-05-08 15:31:49 +0800

    去除认证->让spring security不拦截请求

--- modules/flowable-ui-common/src/main/java/org/flowable/ui/common/rest/idm/remote/RemoteAccountResource.java
+++ modules/flowable-ui-common/src/main/java/org/flowable/ui/common/rest/idm/remote/RemoteAccountResource.java
@@ -19,6 +19,7 @@ import org.flowable.ui.common.model.GroupRepresentation;
 import org.flowable.ui.common.model.RemoteGroup;
 import org.flowable.ui.common.model.RemoteUser;
 import org.flowable.ui.common.model.UserRepresentation;
+import org.flowable.ui.common.security.DefaultPrivileges;
 import org.flowable.ui.common.security.SecurityUtils;
 import org.flowable.ui.common.service.exception.NotFoundException;
 import org.flowable.ui.common.service.idm.RemoteIdmService;
@@ -39,27 +40,40 @@ public class RemoteAccountResource {
      */
     @RequestMapping(value = "/rest/account", method = RequestMethod.GET, produces = "application/json")
     public UserRepresentation getAccount() {
-        UserRepresentation userRepresentation = null;
-        String currentUserId = SecurityUtils.getCurrentUserId();
-        if (currentUserId != null) {
-            RemoteUser remoteUser = remoteIdmService.getUser(currentUserId);
-            if (remoteUser != null) {
-                userRepresentation = new UserRepresentation(remoteUser);
+        UserRepresentation userRepresentation = new UserRepresentation();
+        userRepresentation.setFirstName("admin");
+        userRepresentation.setLastName("admin");
+        userRepresentation.setFullName("admin");
+        userRepresentation.setId("admin");
+        List<String> pris = new ArrayList<>();
+        pris.add(DefaultPrivileges.ACCESS_MODELER);
+        pris.add(DefaultPrivileges.ACCESS_IDM);
+        pris.add(DefaultPrivileges.ACCESS_ADMIN);
+        pris.add(DefaultPrivileges.ACCESS_TASK);
+        pris.add(DefaultPrivileges.ACCESS_REST_API);
+        userRepresentation.setPrivileges(pris);
 
-                if (remoteUser.getGroups() != null && remoteUser.getGroups().size() > 0) {
-                    List<GroupRepresentation> groups = new ArrayList<>();
-                    for (RemoteGroup remoteGroup : remoteUser.getGroups()) {
-                        groups.add(new GroupRepresentation(remoteGroup));
-                    }
-                    userRepresentation.setGroups(groups);
-                }
-
-                if (remoteUser.getPrivileges() != null && remoteUser.getPrivileges().size() > 0) {
-                    userRepresentation.setPrivileges(remoteUser.getPrivileges());
-                }
-
-            }
-        }
+//        UserRepresentation userRepresentation = null;
+//        String currentUserId = SecurityUtils.getCurrentUserId();
+//        if (currentUserId != null) {
+//            RemoteUser remoteUser = remoteIdmService.getUser(currentUserId);
+//            if (remoteUser != null) {
+//                userRepresentation = new UserRepresentation(remoteUser);
+//
+//                if (remoteUser.getGroups() != null && remoteUser.getGroups().size() > 0) {
+//                    List<GroupRepresentation> groups = new ArrayList<>();
+//                    for (RemoteGroup remoteGroup : remoteUser.getGroups()) {
+//                        groups.add(new GroupRepresentation(remoteGroup));
+//                    }
+//                    userRepresentation.setGroups(groups);
+//                }
+//
+//                if (remoteUser.getPrivileges() != null && remoteUser.getPrivileges().size() > 0) {
+//                    userRepresentation.setPrivileges(remoteUser.getPrivileges());
+//                }
+//
+//            }
+//        }
 
         if (userRepresentation != null) {
             return userRepresentation;
